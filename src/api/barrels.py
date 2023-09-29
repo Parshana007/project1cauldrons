78ad5848e2fc86ccd133e3b8765638514156214c
Parshana007 <parshana007@gmail.com>
2023-09-28 22:25:44 -0700

    updated barrels and bottler

--- src/api/barrels.py
+++ src/api/barrels.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter, Depends
 from pydantic import BaseModel
 from src.api import auth
+import sqlalchemy
+from src import database as db
 
 router = APIRouter(
     prefix="/barrels",
@@ -9,7 +11,7 @@ router = APIRouter(
 )
 
 class Barrel(BaseModel):
-    sku: str
+    sku: str 
 
     ml_per_barrel: int
     potion_type: list[int]
@@ -18,11 +20,33 @@ class Barrel(BaseModel):
     quantity: int
 
 @router.post("/deliver")
-def post_deliver_barrels(barrels_delivered: list[Barrel]):
+def post_deliver_barrels(barrels_delivered: list[Barrel]): #gives me a total num of barrels to be purchased
     """ """
     print(barrels_delivered)
 
+    #The deliver should be adding ml and subtracting gold. But should be based on how much gold and ml you already have
+  
+    with db.engine.begin() as connection:
+        result = connection.execute(sqlalchemy.text("SELECT gold FROM global_inventory"))
+
+        gold_row = result.first()
+        gold_amount = gold_row.gold
+
+    #get total red_ml from Barrel
+    for Barrel in barrels_delivered:
+        barrel_red_ml += Barrel.ml_per_barrel
+        gold_amount -= Barrel.price
+    
+    
+
+
+    with db.engine.begin() as connection:
+        connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_red_ml = barrel_red_ml"))
+
+    with db.engine.begin() as connection:
+        connection.execute(sqlalchemy.text("UPDATE global_inventory SET gold = gold_amount"))
     return "OK"
+
 
 # Gets called once a day
 @router.post("/plan")
@@ -30,9 +54,32 @@ def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
     """ """
     print(wholesale_catalog)
 
+    #decision logic (like the conditional) should go into the plan, since that is where you do the planning
+    #getting the num_red_potions
+    with db.engine.begin() as connection:
+        result = connection.execute(sqlalchemy.text("SELECT num_red_potions FROM global_inventory"))
+
+        red_potions_row = result.first()
+        nums_red_potions = red_potions_row.num_red_potions
+
+    #getting the gold total    
+    with db.engine.begin() as connection:
+        result = connection.execute(sqlalchemy.text("SELECT gold FROM global_inventory"))
+
+        gold_row = result.first()
+        gold_amount = gold_row.gold
+    
+    total_barrels = 0
+
+    if nums_red_potions < 10:
+        for Barrel in wholesale_catalog:
+            if gold_amount >= Barrel.price:
+                total_barrels += 1
+                gold_amount -= Barrel.price
+
     return [
         {
             "sku": "SMALL_RED_BARREL",
-            "quantity": 1,
+            "quantity": total_barrels,
         }
     ]
--- src/api/bottler.py
+++ src/api/bottler.py
@@ -2,6 +2,9 @@ from fastapi import APIRouter, Depends
 from enum import Enum
 from pydantic import BaseModel
 from src.api import auth
+import sqlalchemy
+from src import database as db
+
 
 router = APIRouter(
     prefix="/bottler",
@@ -18,6 +21,25 @@ def post_deliver_bottles(potions_delivered: list[PotionInventory]):
     """ """
     print(potions_delivered)
 
+    with db.engine.begin() as connection:
+        result = connection.execute(sqlalchemy.text("SELECT num_red_ml FROM global_inventory"))
+
+        num_red_ml_row = result.first()
+        num_red_ml_total = num_red_ml_row.num_red_ml
+
+    total_red_potions = num_red_ml_total // 100
+
+    num_red_ml_total -= total_red_potions * 100
+
+    # total_red_potions potion_delivered[0].quantity
+    
+    with db.engine.begin() as connection:
+        connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_red_ml = num_red_ml_total"))
+
+    with db.engine.begin() as connection:
+        connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_red_potions = total_red_potions"))
+    
+
     return "OK"
 
 # Gets called 4 times a day
@@ -33,9 +55,20 @@ def get_bottle_plan():
 
     # Initial logic: bottle all barrels into red potions.
 
+    with db.engine.begin() as connection:
+        result = connection.execute(sqlalchemy.text("SELECT num_red_ml FROM global_inventory"))
+
+        num_red_ml_row = result.first()
+        num_red_ml_total = num_red_ml_row.num_red_ml
+
+    total_red_potions = num_red_ml_total // 100
+
+    if total_red_potions == 0:
+        return [] #not enough ml to make a potion
+
     return [
             {
                 "potion_type": [100, 0, 0, 0],
-                "quantity": 5,
+                "quantity": total_red_potions,
             }
         ]
