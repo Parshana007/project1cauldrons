6b43596c319aaa7472b0f79ca3c4360c49821826
Parshana007 <parshana007@gmail.com>
2023-10-04 22:48:41 -0700

    added logic to consider blue and green potions

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -16,20 +16,45 @@ def get_catalog():
     # Can return a max of 20 items.
     # query database for num of red potions in DB
     with db.engine.begin() as connection:
-        result = connection.execute(sqlalchemy.text("SELECT num_red_potions FROM global_inventory"))
+        result = connection.execute(sqlalchemy.text("SELECT num_red_potions, num_green_potions, num_blue_potions FROM global_inventory"))
 
-        red_potions = result.first()
-        nums_red_potions = red_potions.num_red_potions
+        potion_data = result.first()
+    
+    nums_red_potions = potion_data.num_red_potions
+    nums_green_potions = potion_data.num_green_potions
+    nums_blue_potions = potion_data.num_blue_potions
 
-    if nums_red_potions == 0:
+    total_potions = nums_red_potions + nums_green_potions + nums_blue_potions
+
+    if total_potions == 0:
         return []
+    
+    potion_list = []
 
-    return [
-            {
+    if nums_red_potions != 0:
+        potion_list.append({ 
                 "sku": "RED_POTION_0",
                 "name": "red potion",
                 "quantity": nums_red_potions,
                 "price": 50,
                 "potion_type": [100, 0, 0, 0],
-            }
-        ]
+        })
+    if nums_green_potions != 0:
+        potion_list.append({ 
+                "sku": "GREEN_POTION_0",
+                "name": "green potion",
+                "quantity": nums_green_potions,
+                "price": 50,
+                "potion_type": [0, 100, 0, 0], # ! What does this 100 mean?????
+        })
+    if nums_blue_potions != 0:
+        potion_list.append({ 
+                "sku": "BLUE_POTION_0",
+                "name": "blue potion",
+                "quantity": nums_blue_potions,
+                "price": 50,
+                "potion_type": [0, 0, 100, 0],
+        })
+
+
+    return potion_list
