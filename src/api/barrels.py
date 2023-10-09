390f7ae852647c5a5d98472eb8cff2d534d0dc7f
Parshana007 <parshana007@gmail.com>
2023-10-09 12:28:47 -0700

    don't buy red barrel

--- src/api/barrels.py
+++ src/api/barrels.py
@@ -96,9 +96,9 @@ def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
             if quantity_to_purchase > barrel.quantity:
                 quantity_to_purchase = barrel.quantity
 
-            if "RED" in barrel.sku and nums_red_potions < 2:
-                total_red_barrels += quantity_to_purchase
-                gold_amount -= barrel.price * quantity_to_purchase
+            # if "RED" in barrel.sku and nums_red_potions < 2:
+            #     total_red_barrels += quantity_to_purchase
+            #     gold_amount -= barrel.price * quantity_to_purchase
             elif "GREEN" in barrel.sku and nums_green_potions < 2:
                 total_green_barrels += quantity_to_purchase
                 gold_amount -= barrel.price * quantity_to_purchase
