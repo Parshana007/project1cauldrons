4f647b632044c69a3843377834a4a61565fc12b7
Parshana007 <parshana007@gmail.com>
2023-10-15 19:45:24 -0700

    got rid of truthiness part of table

--- src/api/carts.py
+++ src/api/carts.py
@@ -32,7 +32,7 @@ def create_cart(new_cart: NewCart):
         result = connection.execute(
             sqlalchemy.text(
                 """
-                INSERT INTO carts (customer_name, can_buy)
+                INSERT INTO carts (customer_name)
                 VALUES (:customer_name)
                 RETURNING cart_id"""
         ), [{"customer_name" : new_cart.customer}]).scalar()
