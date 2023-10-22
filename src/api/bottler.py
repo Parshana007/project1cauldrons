55958912158cc87caad1e2448a791864627a3b74
Parshana007 <parshana007@gmail.com>
2023-10-21 18:12:32 -0700

    added extra if in bottler to make more variety of potions

--- src/api/bottler.py
+++ src/api/bottler.py
@@ -83,7 +83,7 @@ def get_bottle_plan():
     total_quantity_make = 0
 
     for potion in quantity_potions_result:
-        if total_quantity_sum < 6:
+        if total_quantity_sum < 6 and potion.quantity < 2:
             print("get_bottle_plan: potion ",potion)
             while potion.red_ml <= num_red_ml and potion.green_ml <= num_green_ml and potion.blue_ml <= num_blue_ml and potion.dark_ml <= num_dark_ml and total_quantity_make < 6:
 
