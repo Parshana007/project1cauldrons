46e701c9ad3f7c3f14b489f45676b1dd51161e59
Parshana007 <parshana007@gmail.com>
2023-11-06 19:08:21 -0800

    accidentally was displaying 7 not 6

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -32,9 +32,8 @@ def get_catalog():
 
     for potion in potion_inventory_data:
         potion_quantity = potion.quantity
-        # print("potion_total_displayed", potion_total_displayed)
 
-        if potion_quantity != 0 and potion_total_displayed < 7:
+        if potion_quantity != 0 and potion_total_displayed < 6:
             potion_total_displayed += 1
             potion_list.append({
                 "sku": potion.sku,
